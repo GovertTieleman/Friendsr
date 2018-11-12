de015755230cf08ab6511ac916bd27166361a6c6
Govert Tieleman <govert.tielema@gmail.com>
2018-11-12 14:58:06 +0100

    #TODO connecting layout to class

--- app/src/main/java/com/example/govert/friendsr/MainActivity.java
+++ app/src/main/java/com/example/govert/friendsr/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.govert.friendsr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    static ArrayList<Friend> friends = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        friends.add(new Friend("Arya", "Not a boy.", this.getResources().getIdentifier("arya", "drawable", this.getPackageName())));
     }
 }
