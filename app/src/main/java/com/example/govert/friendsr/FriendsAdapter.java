8dc62c2f317654a07b7248b25d2f7abf651ecc4a
Govert Tieleman <govert.tielema@gmail.com>
2018-11-13 15:54:12 +0100

    gridView implemented

--- app/src/main/java/com/example/govert/friendsr/FriendsAdapter.java
+++ app/src/main/java/com/example/govert/friendsr/FriendsAdapter.java
@@ -1,25 +1,44 @@
 package com.example.govert.friendsr;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FriendsAdapter extends ArrayAdapter<Friend> {
-    private List<Friend> friends;
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
-        }
-        convertView.findViewById();
-        return convertView;
-    }
+    private Context mContext;
+    private List<Friend> friendsList = new ArrayList<>();
 
     public FriendsAdapter(Context context, int resource, List<Friend> objects) {
-        this.friends = objects;
+        super(context, resource, objects);
+        mContext = context;
+        friendsList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View gridItem = convertView;
+
+        if (gridItem == null) {
+            gridItem = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+
+        Friend currentFriend =  friendsList.get(position);
+
+        ImageView image = (ImageView)gridItem.findViewById(R.id.imageView_friend);
+        image.setImageResource(currentFriend.getDrawableId());
+
+        TextView name = (TextView) gridItem.findViewById(R.id.textView_name);
+        name.setText(currentFriend.getName());
+
+        return gridItem;
     }
 }
--- app/src/main/java/com/example/govert/friendsr/MainActivity.java
+++ app/src/main/java/com/example/govert/friendsr/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.govert.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    static ArrayList<Friend> friends = new ArrayList<>();
+    private GridView gridView;
+    private FriendsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        friends.add(new Friend("Arya", "Not a boy.", this.getResources().getIdentifier("arya", "drawable", this.getPackageName())));
+
+        gridView = (GridView) findViewById(R.id.friends_list);
+        ArrayList<Friend> friendsList = new ArrayList<>();
+        friendsList.add(new Friend("Arya", "Not a boy.", R.drawable.arya));
+        friendsList.add(new Friend("Cersei", "Evil, but loves her children.", R.drawable.cersei));
+        friendsList.add(new Friend("Daenerys", "Mother of Dragons.", R.drawable.daenerys));
+        friendsList.add(new Friend("Jaime", "Known as the Kingslayer.", R.drawable.jaime));
+        friendsList.add(new Friend("Jon", "Too noble for his own good.", R.drawable.jon));
+        friendsList.add(new Friend("Jorah", "Has a tragic love life.", R.drawable.jorah));
+        friendsList.add(new Friend("Margaery", "Princess of Highgarden.", R.drawable.margaery));
+        friendsList.add(new Friend("Melisandre", "Loyal servant of the Lord of Light.", R.drawable.melisandre));
+        friendsList.add(new Friend("Sansa", "Lady of Winterfell.", R.drawable.sansa));
+        friendsList.add(new Friend("Tyrion", "His wit and cunning makes up for his dwarfish posture.", R.drawable.tyrion));
+
+
+        adapter = new FriendsAdapter(this, 0, friendsList);
+        gridView.setAdapter(adapter);
     }
 }
